bcfc408e80488725836d0840466fe12970b0f381
Taha Tuna <119931873+TahaTuna1@users.noreply.github.com>
2025-03-13 07:42:24 +0300

    Initial Commit

---
+++ sevenapps-case/sevenapps_caseApp.swift
@@ -0,0 +1,17 @@
+//
+//  sevenapps_caseApp.swift
+//  sevenapps-case
+//
+//  Created by Taha Tuna on 13.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct sevenapps_caseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
